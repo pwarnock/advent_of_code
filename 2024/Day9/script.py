00830479ab194fc433ff6b371a30340eb052933e
Pete Warnock <github@peterwarnock.com>
2024-12-09 15:25:57 -0800

    Two-Pointer Approach was just a little faster

--- 2024/Day9/script.py
+++ 2024/Day9/script.py
@@ -7,46 +7,33 @@ with open('input.txt', 'r') as f:
 # diskmap = [int(char) for char in '2333133121414131402']
 # print('Initial diskmap:', ''.join(map(str, diskmap)))  
 
-blocks = []
-blockmap = []
-file_id = 0
+blockmap = []  
 
-for k, v in enumerate(diskmap):
-    if k % 2 == 0:
-        for i in range(v):
-            blockmap.append(file_id)
-        file_id +=1
-    else:
-        free_space = k
-        for i in range(v):
-            blockmap.append('.')
-
-# print('Initial blockmap:', ''.join(map(str, blockmap)))  
-iteration = 0
-
-while not re.match(r'^\d+\.+$', ''.join(map(str, blockmap))):
-    print(f"\nIteration {iteration}")  
-
-
-    # Find rightmost number and leftmost dot  
-    rightmost_number_idx = None  
-    leftmost_dot_idx = None  
-
-    for i in range(len(blockmap)):  
-        if blockmap[i] == '.' and leftmost_dot_idx is None:  
-            leftmost_dot_idx = i  
-        if blockmap[i] != '.' and leftmost_dot_idx is not None:  
-            rightmost_number_idx = i    
-    # If we found a number after a dot, swap them  
-    if rightmost_number_idx is not None and leftmost_dot_idx is not None:  
-        left, right = leftmost_dot_idx, rightmost_number_idx
-        blockmap[left], blockmap[right] =  blockmap[right], blockmap[left]  
+# Create initial blockmap  
+for k, v in enumerate(diskmap):  
+    if k % 2 == 0:  
+        blockmap.extend([k // 2] * v)  # Use k // 2 as file_id  
     else:  
-        break  # No more swaps needed   
-    # print('Current blockmap:', ''.join(map(str, blockmap)))  
-    iteration += 1  
+        blockmap.extend(['.'] * v)  
 
-# print('Final blockmap:', ''.join(map(str, blockmap)))  
+print('Initial blockmap:', ''.join(map(str, blockmap)))  
+
+# Two-pointer approach to rearrange  
+left = 0  
+right = len(blockmap) - 1  
+
+while left < right:  
+    # Move left pointer to the right until we find a dot  
+    while left < right and isinstance(blockmap[left], int):  
+        left += 1  
+    # Move right pointer to the left until we find a number  
+    while left < right and blockmap[right] == '.':  
+        right -= 1  
+    # Swap if left is still less than right  
+    if left < right:  
+        blockmap[left], blockmap[right] = blockmap[right], blockmap[left]  
+
+print('Final blockmap:', ''.join(map(str, blockmap)))  
 
 def calculate_checksum(blockmap):
     checksum = 0
